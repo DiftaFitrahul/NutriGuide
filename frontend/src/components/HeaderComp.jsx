89e925380198ba091dfeb0f5a5b2c18c1bcc7932
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2024-05-12 18:51:56 +0700

    feat: divide 3 section homepage

--- frontend/src/components/HeaderComp.jsx
+++ frontend/src/components/HeaderComp.jsx
@@ -1,6 +1,6 @@
 export default function HeaderComp({ title }) {
   return (
-    <nav className="flex justify-center items-center w-screen h-[70px] fixed left-0 z-[999]  shadow-xl">
+    <nav className="bg-white flex justify-center items-center w-screen h-[70px] fixed left-0 z-[999]  shadow-xl">
       <div className="text-black text-2xl font-bold">{title}</div>
     </nav>
   );
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import HeaderComp from "@/components/HeaderComp";
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,9 +11,37 @@ export default function Home() {
       </Head>
       <main>
         <HeaderComp title="Home" />
-        <div className="flex flex-col justify-center items-center bg-white min-w-full min-h-screen">
-          <div className="block md:flex justify-between w-full h-full ">
-            Halo bang disini
+        <div className="flex flex-col justify-center items-center bg-white min-w-full min-h-screen ">
+          <div className="flex flex-row justify-between bg-blue-50 w-full h-screen pt-[70px]">
+            <div className="flex-1 flex flex-col pl-[20px] items-start min-w-[120px] max-w-[300px]  border-r-2 border-gray">
+              <h1 className="text-black font-bold pb-[10px] text-2xl pt-[30px]">
+                Menu
+              </h1>
+              <button className="text-black text-lg" onClick={() => {}}>
+                Home
+              </button>
+              <button className="text-black text-lg" onClick={() => {}}>
+                Profile
+              </button>
+              <button className="text-black text-lg" onClick={() => {}}>
+                Recomender
+              </button>
+              <button className="text-black text-lg" onClick={() => {}}>
+                Bookmark
+              </button>
+              <div>Username</div>
+            </div>
+            <div className="flex flex-auto flex-row ">
+              <Image
+                src={"/Data_contoh.png"}
+                alt="email"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div className="flex flex-col flex-1 max-w-[300px] min-w-[200px] bg-blue-500">
+              Makan nasi
+            </div>
           </div>
         </div>
       </main>
