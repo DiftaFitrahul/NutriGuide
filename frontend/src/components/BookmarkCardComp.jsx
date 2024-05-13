b345312af5f5a11a07670c31295a3269b72d9844
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2024-05-13 13:53:37 +0700

    feat: create component for bookmark card

---
+++ frontend/src/components/BookmarkCardComp.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+export default function BookmarkCard() {
+  //   const IDR = new Intl.NumberFormat("id-ID", {
+  //     style: "currency",
+  //     currency: "IDR",
+  //   });
+  //   const maxLengthTitle = 17;
+  //   const trimmedTitle =
+  //     title.length > maxLengthTitle
+  //       ? `${title.slice(0, maxLengthTitle)}...`
+  //       : title;
+
+  //   const maxLengthSubtitle = 100;
+  //   const trimmedSubtitle =
+  //     subtitle.length > maxLengthSubtitle
+  //       ? `${subtitle.slice(0, maxLengthSubtitle)}...`
+  //       : subtitle;
+
+  return (
+    <div className="flex flex-col w-[270px]   shadow-xl  bg-white rounded-3xl ">
+      <Image
+        src={"/Data_contoh.png"}
+        alt="coba"
+        width={300}
+        height={300}
+        unoptimized={true}
+        objectFit="cover"
+        className="self-center  rounded-xl min-h-[250px] max-h-[250px]"
+      />
+      <div className="flex flex-row items-start mt-4 mx-3 mb-3">
+        <div className="flex flex-col items-start ">
+          <p className="text-black font-bold text-[17px]">
+            Nasi gorang yang lezat dan nikmat
+          </p>
+          <p className="text-black  text-[15px]">kalorinya 110</p>
+        </div>
+        <button className="ml-[10px]">
+          <Image
+            src={"/bookmark_icon.png"}
+            alt="coba"
+            width={40}
+            height={40}
+            unoptimized={true}
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/bookmark/bookmark_section.jsx
+++ frontend/src/pages/bookmark/bookmark_section.jsx
@@ -1,3 +1,4 @@
+import BookmarkCard from "@/components/BookmarkCardComp";
 import FoodContentComp from "@/components/FoodContectComp";
 import TrendingComp from "@/components/TrendingComp";
 
@@ -5,31 +6,7 @@ export default function BookmarkSection() {
   return (
     <>
       <div className="flex flex-auto flex-col w-[50px] h-full  overflow-auto">
-        <FoodContentComp />
-        <FoodContentComp />
-        <FoodContentComp />
-        <FoodContentComp />
-        <FoodContentComp />
-        <FoodContentComp />
-        <FoodContentComp />
-        <FoodContentComp />
-      </div>
-      <div className="flex flex-col flex-1 max-w-[370px] min-w-[200px]  ">
-        <h1 className="font-semibold text-black py-[10px] ml-[40px] mt-[20px]">
-          Trending
-        </h1>
-        <div className="flex flex-col h-3/6 w-[150px] md:w-[250px] px-[10px]  ml-[20px] lg:ml-[40px]  rounded-3xl border border-black border-opacity-50 overflow-auto">
-          <TrendingComp
-            onClickMenu={(data) => {
-              console.log("makan nasi " + data);
-            }}
-          />
-          <TrendingComp />
-          <TrendingComp />
-          <TrendingComp />
-          <TrendingComp />
-          <TrendingComp />
-        </div>
+        <BookmarkCard />
       </div>
     </>
   );
